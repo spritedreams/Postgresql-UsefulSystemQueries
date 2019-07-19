db836541594010672cf6b61d895773e7c7a6cff8
Christina Skowronski <christina.skowronski@smartwarehousing.com>
2019-07-19 15:12:42 -0500

    missed a couple

---
+++ row_count.sql
@@ -0,0 +1,9 @@
+--approx row count for all user tables
+SELECT
+  nspname AS schemaname,relname,reltuples
+FROM pg_class C
+LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+WHERE
+  nspname NOT IN ('pg_catalog', 'information_schema') AND
+  relkind='r'
+ORDER BY reltuples DESC;
